5dc9789c030103cd1a6a49c28901dd51b5415532
Rob O'Doherty <rodoherty1@gmail.com>
2015-04-25 23:57:00 +0100

    Nearly there!

--- week2/calculator/src/main/scala/calculator/Calculator.scala
+++ week2/calculator/src/main/scala/calculator/Calculator.scala
@@ -1,33 +1,52 @@
 package calculator
 
+import scala.collection.immutable.Iterable
+
 sealed abstract class Expr
+
 final case class Literal(v: Double) extends Expr
+
 final case class Ref(name: String) extends Expr
+
 final case class Plus(a: Expr, b: Expr) extends Expr
+
 final case class Minus(a: Expr, b: Expr) extends Expr
+
 final case class Times(a: Expr, b: Expr) extends Expr
+
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
   def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
 
-    /*
-    Below you can see that I have removed k from namedExpressions.
-    I am reading some of the tips on the coursera page (https://class.coursera.org/reactive-002/forum/thread?thread_id=257)
-    and they are saying things like "look at the map and notice what is in there that shouldn't be in there".
-    Also in the lecture, Martin said something about seeing if the list of observers of an expression contains the
-    expression itself.
-
-    E.g. a = b + 1 and b = a + 1 then a = (a + 1) + 1 which is invalid.
-
-    So, do I need to remove k from namedExpressions and then evaluate it out and make sure that k never comes up again?
-    Gotta hit the hay now !!!!
-     */
     namedExpressions.map {
-      case (k, v) => (k, Signal { eval(v(), namedExpressions - k) })
-    }
+      case (k, v) => {
+        v() match {
+          case Ref(n) => if (n == k) (k, Signal(Literal(Double.NaN))) else (k, v)
+          case _ => (k, v)
+        }
+      }
+    } map {
 
+      // When there is a change in the UI, the code inside Var { } is reevaluated.
+      case (k, v) => (k, Var { eval(v(), removeSelfRefs(k, namedExpressions)) })
+    }
   }
+
+  def removeSelfRefs(name: String, refs: Map[String, Signal[Expr]]): Map[String, Signal[Expr]] = {
+    refs.map {
+      case (k, v) if k == name => (k, Signal[Expr](Literal(Double.NaN)))
+      case (k, v) => (k, v)
+    } map {
+      case (k, v) => {
+        v() match {
+          case Ref(n) if n == name => (k, Signal[Expr](Literal(Double.NaN)))
+          case _ => (k, v)
+        }
+      }
+    }
+  }
+
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
     expr match {
@@ -41,11 +60,12 @@ object Calculator {
     }
   }
 
+
   /** Get the Expr for a referenced variables.
-   *  If the variable is not known, returns a literal NaN.
-   */
+    * If the variable is not known, returns a literal NaN.
+    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]): Expr = {
+                               references: Map[String, Signal[Expr]]): Expr = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
